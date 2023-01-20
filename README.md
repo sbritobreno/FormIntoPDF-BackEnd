7ad029b15681b13a432aca84b4fb249062302c45
Breno Brito <breno.silvabrito@hotmail.com>
2023-01-20 18:40:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FormIntoPDF_Back-End
+
+Back End for the Form Into PDF Web and Mobile Application
---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Config JSON response
+app.use(express.json());
+
+// Config URLenconded
+
+// Solve CORS
+app.use(cors({ credentials: true, origin: process.env.CLIENT_ORIGIN }));
+
+// Public folder for images
+app.use(express.static("public"));
+
+// Routes
+
+
+app.listen(5000);
