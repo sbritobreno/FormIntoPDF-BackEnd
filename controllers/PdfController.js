e91c26d595ffa45908129e9152eb5c5159bc63d4
Breno Brito <breno.silvabrito@hotmail.com>
2023-05-05 11:54:48 +0100

    Format tempplate pages and create function to download only reinstatementSheet

--- controllers/PdfController.js
+++ controllers/PdfController.js
@@ -118,271 +118,136 @@ module.exports = class PdfController {
       return;
     }
 
+    function getPath(fileName) {
+      return path.join(__dirname, `../public/pdfTemplate/${fileName}`);
+    }
+
+    function generatePdfPage(templatePath, data) {
+      const template = fs.readFileSync(templatePath, "utf8");
+      const compiled = hbs.compile(template);
+      const html = compiled(data);
+      return html;
+    }
+
     try {
       const data = document.toJSON();
-      console.log(data);
       const options = { format: "A4" };
 
       //1. Site Attendance 1
-      const templatePage1Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/siteAttendance1.hbs"
-      );
-      const templatePage1 = fs.readFileSync(templatePage1Path, "utf8");
-      const compiledPage1 = hbs.compile(templatePage1);
-      const htmlPage1 = compiledPage1(data);
+      const templatePage1Path = getPath("siteAttendance1.hbs");
+      const htmlPage1 = generatePdfPage(templatePage1Path, data);
 
       //2. Site Attendance 2
-      const templatePage2Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/siteAttendance2.hbs"
-      );
-      const templatePage2 = fs.readFileSync(templatePage2Path, "utf8");
-      const compiledPage2 = hbs.compile(templatePage2);
-      const htmlPage2 = compiledPage2(data);
+      const templatePage2Path = getPath("siteAttendance2.hbs");
+      const htmlPage2 = generatePdfPage(templatePage2Path, data);
 
       //3. Job Safety Plan
-      const templatePage3Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/jobSafetyPlan.hbs"
-      );
-      const templatePage3 = fs.readFileSync(templatePage3Path, "utf8");
-      const compiledPage3 = hbs.compile(templatePage3);
-      const htmlPage3 = compiledPage3(data);
+      const templatePage3Path = getPath("jobSafetyPlan.hbs");
+      const htmlPage3 = generatePdfPage(templatePage3Path, data);
 
       //4. Method Statement 1
-      const templatePage4Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement1.hbs"
-      );
-      const templatePage4 = fs.readFileSync(templatePage4Path, "utf8");
-      const compiledPage4 = hbs.compile(templatePage4);
-      const htmlPage4 = compiledPage4(data);
+      const templatePage4Path = getPath("methodStatement1.hbs");
+      const htmlPage4 = generatePdfPage(templatePage4Path, data);
 
       //5. Method Statement 2
-      const templatePage5Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement2.hbs"
-      );
-      const templatePage5 = fs.readFileSync(templatePage5Path, "utf8");
-      const compiledPage5 = hbs.compile(templatePage5);
-      const htmlPage5 = compiledPage5(data);
+      const templatePage5Path = getPath("methodStatement2.hbs");
+      const htmlPage5 = generatePdfPage(templatePage5Path, data);
 
       //6. Further Hazards
-      const templatePage6Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/furtherHazards.hbs"
-      );
-      const templatePage6 = fs.readFileSync(templatePage6Path, "utf8");
-      const compiledPage6 = hbs.compile(templatePage6);
-      const htmlPage6 = compiledPage6(data);
+      const templatePage6Path = getPath("furtherHazards.hbs");
+      const htmlPage6 = generatePdfPage(templatePage6Path, data);
 
       //7. Site Specific Requirements
-      const templatePage7Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/siteSpecificRequirements.hbs"
-      );
-      const templatePage7 = fs.readFileSync(templatePage7Path, "utf8");
-      const compiledPage7 = hbs.compile(templatePage7);
-      const htmlPage7 = compiledPage7(data);
+      const templatePage7Path = getPath("siteSpecificRequirements.hbs");
+      const htmlPage7 = generatePdfPage(templatePage7Path, data);
 
       //8. Method Statement 3
-      const templatePage8Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement3.hbs"
-      );
-      const templatePage8 = fs.readFileSync(templatePage8Path, "utf8");
-      const compiledPage8 = hbs.compile(templatePage8);
-      const htmlPage8 = compiledPage8(data);
+      const templatePage8Path = getPath("methodStatement3.hbs");
+      const htmlPage8 = generatePdfPage(templatePage8Path, data);
 
       //9. Method Statement 4
-      const templatePage9Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement4.hbs"
-      );
-      const templatePage9 = fs.readFileSync(templatePage9Path, "utf8");
-      const compiledPage9 = hbs.compile(templatePage9);
-      const htmlPage9 = compiledPage9(data);
+      const templatePage9Path = getPath("methodStatement4.hbs");
+      const htmlPage9 = generatePdfPage(templatePage9Path, data);
 
       //10. Method Statement 5
-      const templatePage10Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement5.hbs"
-      );
-      const templatePage10 = fs.readFileSync(templatePage10Path, "utf8");
-      const compiledPage10 = hbs.compile(templatePage10);
-      const htmlPage10 = compiledPage10(data);
+      const templatePage10Path = getPath("methodStatement5.hbs");
+      const htmlPage10 = generatePdfPage(templatePage10Path, data);
 
       //11. Method Statement 6
-      const templatePage11Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement6.hbs"
-      );
-      const templatePage11 = fs.readFileSync(templatePage11Path, "utf8");
-      const compiledPage11 = hbs.compile(templatePage11);
-      const htmlPage11 = compiledPage11(data);
+      const templatePage11Path = getPath("methodStatement6.hbs");
+      const htmlPage11 = generatePdfPage(templatePage11Path, data);
 
       //12. Method Statement 7
-      const templatePage12Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement7.hbs"
-      );
-      const templatePage12 = fs.readFileSync(templatePage12Path, "utf8");
-      const compiledPage12 = hbs.compile(templatePage12);
-      const htmlPage12 = compiledPage12(data);
+      const templatePage12Path = getPath("methodStatement7.hbs");
+      const htmlPage12 = generatePdfPage(templatePage12Path, data);
 
       //13. Method Statement 8
-      const templatePage13Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement8.hbs"
-      );
-      const templatePage13 = fs.readFileSync(templatePage13Path, "utf8");
-      const compiledPage13 = hbs.compile(templatePage13);
-      const htmlPage13 = compiledPage13(data);
+      const templatePage13Path = getPath("methodStatement8.hbs");
+      const htmlPage13 = generatePdfPage(templatePage13Path, data);
 
       //14. Method Statement 9
-      const templatePage14Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement9.hbs"
-      );
-      const templatePage14 = fs.readFileSync(templatePage14Path, "utf8");
-      const compiledPage14 = hbs.compile(templatePage14);
-      const htmlPage14 = compiledPage14(data);
+      const templatePage14Path = getPath("methodStatement9.hbs");
+      const htmlPage14 = generatePdfPage(templatePage14Path, data);
 
       //15. Method Statement 10
-      const templatePage15Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement10.hbs"
-      );
-      const templatePage15 = fs.readFileSync(templatePage15Path, "utf8");
-      const compiledPage15 = hbs.compile(templatePage15);
-      const htmlPage15 = compiledPage15(data);
+      const templatePage15Path = getPath("methodStatement10.hbs");
+      const htmlPage15 = generatePdfPage(templatePage15Path, data);
 
       //16. Method Statement 11
-      const templatePage16Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement11.hbs"
-      );
-      const templatePage16 = fs.readFileSync(templatePage16Path, "utf8");
-      const compiledPage16 = hbs.compile(templatePage16);
-      const htmlPage16 = compiledPage16(data);
+      const templatePage16Path = getPath("methodStatement11.hbs");
+      const htmlPage16 = generatePdfPage(templatePage16Path, data);
 
       //17. Method Statement 12
-      const templatePage17Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement12.hbs"
-      );
-      const templatePage17 = fs.readFileSync(templatePage17Path, "utf8");
-      const compiledPage17 = hbs.compile(templatePage17);
-      const htmlPage17 = compiledPage17(data);
+      const templatePage17Path = getPath("methodStatement12.hbs");
+      const htmlPage17 = generatePdfPage(templatePage17Path, data);
 
       //18. Method Statement 13
-      const templatePage18Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/methodStatement13.hbs"
-      );
-      const templatePage18 = fs.readFileSync(templatePage18Path, "utf8");
-      const compiledPage18 = hbs.compile(templatePage18);
-      const htmlPage18 = compiledPage18(data);
+      const templatePage18Path = getPath("methodStatement13.hbs");
+      const htmlPage18 = generatePdfPage(templatePage18Path, data);
 
       //19. Method Statement 13
-      const templatePage19Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/permitToDigChecklist.hbs"
-      );
-      const templatePage19 = fs.readFileSync(templatePage19Path, "utf8");
-      const compiledPage19 = hbs.compile(templatePage19);
-      const htmlPage19 = compiledPage19(data);
+      const templatePage19Path = getPath("permitToDigChecklist.hbs");
+      const htmlPage19 = generatePdfPage(templatePage19Path, data);
 
-      //20. Daily Plant Inspection
-      const templatePage20Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/dailyPlantInspection1.hbs"
-      );
-      const templatePage20 = fs.readFileSync(templatePage20Path, "utf8");
-      const compiledPage20 = hbs.compile(templatePage20);
-      const htmlPage20 = compiledPage20(data);
+      //20. Daily Plant Inspection 1
+      const templatePage20Path = getPath("dailyPlantInspection1.hbs");
+      const htmlPage20 = generatePdfPage(templatePage20Path, data);
 
-      //21. Daily Plant Inspection
-      const templatePage21Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/dailyPlantInspection2.hbs"
-      );
-      const templatePage21 = fs.readFileSync(templatePage21Path, "utf8");
-      const compiledPage21 = hbs.compile(templatePage21);
-      const htmlPage21 = compiledPage21(data);
+      //21. Daily Plant Inspection 2
+      const templatePage21Path = getPath("dailyPlantInspection2.hbs");
+      const htmlPage21 = generatePdfPage(templatePage21Path, data);
 
-      //22. Daily Plant Inspection
-      const templatePage22Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/nearMissReport.hbs"
-      );
-      const templatePage22 = fs.readFileSync(templatePage22Path, "utf8");
-      const compiledPage22 = hbs.compile(templatePage22);
-      const htmlPage22 = compiledPage22(data);
+      //22. Near Miss Report
+      const templatePage22Path = getPath("nearMissReport.hbs");
+      const htmlPage22 = generatePdfPage(templatePage22Path, data);
 
       //23. Approved Form 1
-      const templatePage23Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/approvedForm1.hbs"
-      );
-      const templatePage23 = fs.readFileSync(templatePage23Path, "utf8");
-      const compiledPage23 = hbs.compile(templatePage23);
-      const htmlPage23 = compiledPage23(data);
+      const templatePage23Path = getPath("approvedForm1.hbs");
+      const htmlPage23 = generatePdfPage(templatePage23Path, data);
 
       //24. Approved Form 2
-      const templatePage24Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/approvedForm2.hbs"
-      );
-      const templatePage24 = fs.readFileSync(templatePage24Path, "utf8");
-      const compiledPage24 = hbs.compile(templatePage24);
-      const htmlPage24 = compiledPage24(data);
+      const templatePage24Path = getPath("approvedForm2.hbs");
+      const htmlPage24 = generatePdfPage(templatePage24Path, data);
 
       //25. Hot Work Permit
-      const templatePage25Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/hotWorkPermit.hbs"
-      );
-      const templatePage25 = fs.readFileSync(templatePage25Path, "utf8");
-      const compiledPage25 = hbs.compile(templatePage25);
-      const htmlPage25 = compiledPage25(data);
+      const templatePage25Path = getPath("hotWorkPermit.hbs");
+      const htmlPage25 = generatePdfPage(templatePage25Path, data);
 
-      //26. Hot Work Permit
-      const templatePage26Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/trafficManagementSLGChecklist1.hbs"
-      );
-      const templatePage26 = fs.readFileSync(templatePage26Path, "utf8");
-      const compiledPage26 = hbs.compile(templatePage26);
-      const htmlPage26 = compiledPage26(data);
+      //26. Traffic Management SLG Checklist 1
+      const templatePage26Path = getPath("trafficManagementSLGChecklist1.hbs");
+      const htmlPage26 = generatePdfPage(templatePage26Path, data);
 
-      //27. Hot Work Permit
-      const templatePage27Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/trafficManagementSLGChecklist2.hbs"
-      );
-      const templatePage27 = fs.readFileSync(templatePage27Path, "utf8");
-      const compiledPage27 = hbs.compile(templatePage27);
-      const htmlPage27 = compiledPage27(data);
+      //27.  Traffic Management SLG Checklist 2
+      const templatePage27Path = getPath("trafficManagementSLGChecklist2.hbs");
+      const htmlPage27 = generatePdfPage(templatePage27Path, data);
 
       //28. Reinstatement Sheet 1
-      const templatePage28Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/reinstatementSheet1.hbs"
-      );
-      const templatePage28 = fs.readFileSync(templatePage28Path, "utf8");
-      const compiledPage28 = hbs.compile(templatePage28);
-      const htmlPage28 = compiledPage28(data);
+      const templatePage28Path = getPath("reinstatementSheet1.hbs");
+      const htmlPage28 = generatePdfPage(templatePage28Path, data);
 
-      //29. Reinstatement Sheet 1
-      const templatePage29Path = path.join(
-        __dirname,
-        "../public/pdfTemplate/reinstatementSheet2.hbs"
-      );
-      const templatePage29 = fs.readFileSync(templatePage29Path, "utf8");
-      const compiledPage29 = hbs.compile(templatePage29);
-      const htmlPage29 = compiledPage29(data);
+      //29. Reinstatement Sheet 2
+      const templatePage29Path = getPath("reinstatementSheet2.hbs");
+      const htmlPage29 = generatePdfPage(templatePage29Path, data);
 
       // Concatenate all HTML pages
       const html =
@@ -438,22 +303,96 @@ module.exports = class PdfController {
   // Download ReinstatementSheet
   static async downloadReinstatementSheet(req, res) {
     const id = req.params.id;
-    const reinstatementSheet = await ReinstatementSheet.findOne({
-      where: { DocumentId: id },
+
+    hbs.registerHelper("inc", function (value) {
+      return parseInt(value) + 1;
     });
 
-    if (!reinstatementSheet) {
+    hbs.registerHelper("lessthen", function (list, n) {
+      return list < n;
+    });
+
+    hbs.registerHelper("range", function (start, end) {
+      var ret = [];
+      for (var i = start; i < end; i++) {
+        ret.push(i);
+      }
+      return ret;
+    });
+
+    const document = await Document.findOne({
+      where: { id: id },
+      include: [
+        { model: SiteAttendance },
+        { model: Hazards },
+        { model: DMSandTMC },
+        { model: Emergencies },
+        { model: TrafficManagementComplianceChecksheet },
+        { model: TrafficManagementSlgChecklist },
+        { model: ApprovedForm },
+        { model: HotWorkPermit },
+        { model: DailyPlantInspection },
+        { model: NearMissReport },
+        { model: FutherHazarsAndControls },
+        { model: MethodStatementsJobInfo },
+        {
+          model: ReinstatementSheet,
+          include: [
+            {
+              model: ReinstatementSheetHoleSequence,
+              include: [{ model: ReinstatementImages }],
+            },
+          ],
+        },
+      ],
+    });
+
+    if (!document) {
       res.status(404).json({
-        message:
-          "Reinstatement sheet not found. It might not have been initialized yet!",
+        message: "Document not found!",
       });
       return;
     }
 
     try {
-      res
-        .status(200)
-        .json({ message: "Reinstatement Sheet downloaded successfully!" });
+      const data = document.toJSON();
+      const options = { format: "A4" };
+
+      // Page 1
+      const templatePath1 = path.join(
+        __dirname,
+        `../public/pdfTemplate/reinstatementSheet1.hbs`
+      );
+      const templatePage1 = fs.readFileSync(templatePath1, "utf8");
+      const compiledPage1 = hbs.compile(templatePage1);
+      const htmlPage1 = compiledPage1(data);
+
+      // Page 2
+      const templatePath2 = path.join(
+        __dirname,
+        `../public/pdfTemplate/reinstatementSheet2.hbs`
+      );
+      const templatePage2 = fs.readFileSync(templatePath2, "utf8");
+      const compiledPage2 = hbs.compile(templatePage2);
+      const htmlPage2 = compiledPage2(data);
+
+      // Concatenate all HTML pages
+      const html = htmlPage1 + htmlPage2;
+
+      pdf
+        .generatePdf({ content: html }, options)
+        .then((pdfBuffer) => {
+          res.setHeader("Content-Type", "application/pdf");
+          res.setHeader(
+            "Content-Disposition",
+            "attachment; filename=reinstatementSheet.pdf"
+          );
+          res.send(pdfBuffer);
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send("Error generating PDF");
+        });
     } catch (error) {
       res.status(500).json({ message: error });
     }
