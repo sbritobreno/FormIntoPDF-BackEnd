feb92176f6d9447c1cdba430d6311fb6be39dfcb
Breno Brito <breno.silvabrito@hotmail.com>
2023-05-23 16:26:13 +0100

    Update env file

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("form2pdf", "root", process.env.DB_PASSWORD, {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 try {
   sequelize.authenticate();
